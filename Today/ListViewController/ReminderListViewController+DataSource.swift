3a8e3ce37e2cd9c7cd2c88411d9dc4f11f181a76
백대홍 <bdh3620@naver.com>
2024-11-05 23:19:48 +0900

    Feat: 뷰컨트롤 구성

--- Today/ListViewController/ReminderListViewController+DataSource.swift
+++ Today/ListViewController/ReminderListViewController+DataSource.swift
@@ -4,5 +4,19 @@
 //
 //  Created by 백대홍 on 11/5/24.
 //
-
+// 해당 익스텐션에서 알림 List에 대해 데이터소스 역할을 하는 모든 동작을 포함
 import UIKit
+
+extension ReminderListViewController {
+    typealias DataSource = UICollectionViewDiffableDataSource<Int, String>
+    typealias SnapShot = NSDiffableDataSourceSnapshot<Int, String>
+    
+    func cellRegistrationHandler(cell: UICollectionViewListCell, indexPath: IndexPath, id: String) {
+        let reminder = Reminder.sampleData[indexPath.item]
+        var contentConfiguration = cell.defaultContentConfiguration()
+        contentConfiguration.text = reminder.title
+        contentConfiguration.secondaryText = reminder.dueDate.dayAndTimeText
+        contentConfiguration.secondaryTextProperties.font = UIFont.preferredFont(forTextStyle: .caption1)
+        cell.contentConfiguration = contentConfiguration
+    }
+}
--- Today/ListViewController/ReminderListViewController.swift
+++ Today/ListViewController/ReminderListViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  ReminderListViewController.swift
 //  Today
 //
 //  Created by 백대홍 on 11/3/24.
@@ -9,8 +9,8 @@ import UIKit
 
 class ReminderListViewController: UICollectionViewController {
     // DataSource, snapShot의 유형을 Int, String으로 설정하는 타입 별칭
-    typealias DataSource = UICollectionViewDiffableDataSource<Int, String>
-    typealias SnapShot = NSDiffableDataSourceSnapshot<Int, String>
+//    typealias DataSource = UICollectionViewDiffableDataSource<Int, String>
+//    typealias SnapShot = NSDiffableDataSourceSnapshot<Int, String>
     
     var dataSource: DataSource!
     
@@ -21,18 +21,21 @@ class ReminderListViewController: UICollectionViewController {
         let listLayout = listLayout()
         collectionView.collectionViewLayout = listLayout
         
+        let cellRegistration = UICollectionView.CellRegistration(handler: cellRegistrationHandler)
+        
         // 셀 등록 구성: 셀 생성 시마다 실행될 클로저를 정의
-        let cellRegistration = UICollectionView.CellRegistration {
-            (cell: UICollectionViewListCell, IndexPath: IndexPath, itemIdentifier: String) in
-            // 인덱스에 해당하는 Reminder 객체 가져오기
-            let reminder = Reminder.sampleData[IndexPath.item]
-            // 셀의 기본 콘텐츠 설정
-            var contentConfiguration = cell.defaultContentConfiguration()
-            // 제목 설정
-            contentConfiguration.text = reminder.title
-            // 셀에 콘텐츠 설정 적용
-            cell.contentConfiguration = contentConfiguration
-        }
+        // 위 코드로 대체
+//        let cellRegistration = UICollectionView.CellRegistration {
+//            (cell: UICollectionViewListCell, IndexPath: IndexPath, itemIdentifier: String) in
+//            // 인덱스에 해당하는 Reminder 객체 가져오기
+//            let reminder = Reminder.sampleData[IndexPath.item]
+//            // 셀의 기본 콘텐츠 설정
+//            var contentConfiguration = cell.defaultContentConfiguration()
+//            // 제목 설정
+//            contentConfiguration.text = reminder.title
+//            // 셀에 콘텐츠 설정 적용
+//            cell.contentConfiguration = contentConfiguration
+//        }
         // 데이터 소스 초기화: 컬렉션 뷰와 셀 등록 클로저를 이용해 구성
         dataSource = DataSource(collectionView: collectionView) {
             (collectionView: UICollectionView, IndexPath: IndexPath, itemIdentifier: String) in
