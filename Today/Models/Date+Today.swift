819a759040903bef9770160e2af10cd26e87bdd9
백대홍 <bdh3620@naver.com>
2024-11-05 23:31:50 +0900

    Feat: 날짜 한글 포맷 변경

--- Today/Models/Date+Today.swift
+++ Today/Models/Date+Today.swift
@@ -9,19 +9,27 @@ import Foundation
 
 extension Date {
     // 현재 날짜를 "날짜 및 시간" 형태의 문자열로 변환해 반환해주는 계산
+    
     var dayAndTimeText: String {
-        //날짜 형식 제외, 시간만 짧게 표시
-        let timeText = formatted(date: .omitted, time: .shortened)
-        //현재 날짜가 오늘이라면 아래와 같은 형태로 반환
+        // 커스텀 포맷을 사용해 날짜 및 시간 문자열 생성
+        let timeText = self.formatted(customFormat)
+
+        // 현재 날짜가 오늘인 경우 "Today at [시간]" 형식으로 반환
         if Locale.current.calendar.isDateInToday(self) {
-            let timeFormat = NSLocalizedString("Today at %@", comment: "Today at time format string")
-            //문자열을 포맷팅해서 반환
+            let timeFormat = NSLocalizedString("오늘은 %@", comment: "Today at time format string")
             return String(format: timeFormat, timeText)
         } else {
-            // 오늘이 아니면 날짜는 월과 일만 표시
-            let dateText = formatted(.dateTime.month(.abbreviated).day())
-            let dateAndTimeFormat = NSLocalizedString("%@ at %@", comment: "Date and time format string")
+            // 오늘이 아닌 경우 날짜와 시간을 포함한 형식으로 반환
+            let dateText = self.formatted(customFormat)
+            let dateAndTimeFormat = NSLocalizedString("%@부터 %@", comment: "Date and time format string")
             return String(format: dateAndTimeFormat, dateText, timeText)
         }
     }
+
 }
+// 커스텀 한글 포맷
+let customFormat = Date.FormatStyle()
+    .month(.wide)
+    .day(.twoDigits)
+    .year(.defaultDigits)
+    .locale(Locale(identifier: "ko_KR"))
