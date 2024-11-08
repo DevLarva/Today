bbed25ea5787f36d576589a832be7cc4f99312be
백대홍 <bdh3620@naver.com>
2024-11-08 19:38:54 +0900

    Feat: 사용자가 버튼을 탭할 때 호출될 함수 정의 완

--- Today/ListViewController/ReminderListViewController+Actions.swift
+++ Today/ListViewController/ReminderListViewController+Actions.swift
@@ -4,5 +4,12 @@
 //
 //  Created by 백대홍 on 11/8/24.
 //
+// reminder 식별자를 저장하는 사용자 지정 버튼을 만들고, 사용자가 버튼을 탭할때 호출할 함수 정의
+import UIKit
 
-import Foundation
+extension ReminderListViewController {
+    @objc func didPressDoneButton(_ sender: RemiderDoneButton) {
+        guard let id = sender.id else { return }
+        completeReminder(withId: id)
+    }
+}
