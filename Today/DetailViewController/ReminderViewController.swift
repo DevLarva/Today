623584b17321003c12dda2a78b2ab5494bca0523
백대홍 <bdh3620@naver.com>
2024-11-10 20:41:20 +0900

    Feat: reminder 상세보기 데이터 소스 설정

--- Today/DetailViewController/ReminderViewController.swift
+++ Today/DetailViewController/ReminderViewController.swift
@@ -8,7 +8,10 @@
 import UIKit
 
 class ReminderViewController: UICollectionViewController {
+    private typealias DataSource = UICollectionViewDiffableDataSource<Int, Row>
+    
     var reminder: Reminder
+    private var dataSource: DataSource!
     
     init(reminder: Reminder) {
         self.reminder = reminder
@@ -17,8 +20,27 @@ class ReminderViewController: UICollectionViewController {
         let listLayout = UICollectionViewCompositionalLayout.list(using: listConfiguration)
         super.init(collectionViewLayout: listLayout)
     }
+    
+    override func viewDidLoad() {
+        super .viewDidLoad()
+        let cellRegistraion = UICollectionView.CellRegistration(handler: cellRegistrationHandler)
+        dataSource = DataSource(collectionView: collectionView) {
+            (collectionView: UICollectionView, IndexPath: IndexPath, itemIdetifier: Row) in
+            return collectionView.dequeueConfiguredReusableCell(using: cellRegistraion, for: IndexPath, item: itemIdetifier)
+        }
+    }
+    
     required init?(coder: NSCoder) {
         fatalError("Always initialize ReminderViewController using init(reminder:)")
+    }
+    
+    func cellRegistrationHandler(cell: UICollectionViewListCell, indexPath: IndexPath, row: Row) {
+        var contentConfiguration = cell.defaultContentConfiguration()
+        contentConfiguration.text = text(for: row)
+        contentConfiguration.textProperties.font = UIFont.preferredFont(forTextStyle: row.textStyle)
+        contentConfiguration.image = row.image
+        cell.contentConfiguration = contentConfiguration
+        cell.tintColor = .darkGray
     }
     
     func text(for row: Row) -> String? {
